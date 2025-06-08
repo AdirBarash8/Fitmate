58a5d8dc54b4efa2dc2a20e201ef38380c7dff7e
hadarzilkha <hadarz2812@gmail.com>
2025-06-08 16:32:18 +0300

    navbar

--- fitness-matcher-frontend/src/pages/Layout.jsx
+++ fitness-matcher-frontend/src/pages/Layout.jsx
@@ -1,12 +1,13 @@
 // src/components/Layout.jsx
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import "../App.css"
 
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <main>
+      <main className="page-content">
         <Outlet /> {/* this will render the child route component */}
       </main>
     </>
